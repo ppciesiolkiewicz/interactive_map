33b88b1bd8567c63658df62ce35661f3e668d91f
krzaczek <krzaczekw@gmail.com>
2014-10-29 12:02:23 +0100

    generalization of models

---
+++ Interactive Map Service/src/main/java/pl/edu/wroc/pwr/service/manager/ModelManager.java
@@ -0,0 +1,9 @@
+package pl.edu.wroc.pwr.service.manager;
+
+import pl.edu.wroc.pwr.service.model.Model;
+
+/**
+ * Created by krzaczek on 29.10.14.
+ */
+public class ModelManager<T extends Model> {
+}
---
+++ Interactive Map Service/src/main/java/pl/edu/wroc/pwr/service/model/Model.java
@@ -0,0 +1,7 @@
+package pl.edu.wroc.pwr.service.model;
+
+/**
+ * Created by krzaczek on 29.10.14.
+ */
+public class Model {
+}
