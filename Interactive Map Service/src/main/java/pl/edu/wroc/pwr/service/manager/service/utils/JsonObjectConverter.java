8e6e44eaa15824607a00bdcde528fb810ecd6713
krzaczek <krzaczekw@gmail.com>
2015-01-17 11:22:50 +0100

    changed return id to return jsonObject wit property ID for items creation

---
+++ Interactive Map Service/src/main/java/pl/edu/wroc/pwr/service/manager/service/utils/JsonObjectConverter.java
@@ -0,0 +1,16 @@
+package pl.edu.wroc.pwr.service.manager.service.utils;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+/**
+ * Created by krzaczek on 17.01.15.
+ */
+public class JsonObjectConverter {
+
+	public JsonElement convert(String propertyName, String value) {
+		JsonObject object = new JsonObject();
+		object.addProperty(propertyName, value);
+		return object;
+	}
+}
